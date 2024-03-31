d9fac68d779855a64903f625ec653d8ed75c3f88
Aditya <adityasid786@gmail.com>
2024-03-31 16:54:49 +0530

    setup module import and added serves static files

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-require("dotenv").config();
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env",
+});
 const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-  res.send(`Hello InstaFood lovers!`);
-});
+app.use(express.static("dist"));
 
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
